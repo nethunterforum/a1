20d40c3dbe5554e8087823caf47f1091ba14ed3a
hitman <nethunterforum78687>
2023-05-20 04:14:30 -0400

    first commit

--- execute.py
+++ execute.py
@@ -1,25 +1,25 @@
 import os
 import sys
-"""
+
 with open('result1.txt', 'a') as file:
     file.write('start\n')
 file.close()
 with open('a.txt') as f:
     for line in f:
-        os.system('python ex.py http://'+line.strip('\n'))
+        os.system('python3.8 ex.py http://'+line.strip('\n'))
         with open('result1.txt', 'a') as file:
             file.write(line.strip('\n')+'\n')
         file.close()
 with open('result1.txt', 'a') as file:
     file.write('fin\n')
 file.close()
-"""
+
 with open('result2.txt', 'a') as file:
     file.write('start\n')
 file.close()
 with open('aa.txt') as f:
     for line in f:
-        os.system('python ex.py http://'+line.strip('\n'))
+        os.system('python3.8 ex.py http://'+line.strip('\n'))
         with open('result1.txt', 'a') as file:
             file.write(line.strip('\n')+'\n')
         file.close()
