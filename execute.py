ba9f3474227f9c91d7f24a39285b6597b3c29eea
hitman <nethunterforum78687>
2023-05-24 09:14:41 -0400

    first commit

--- execute.py
+++ execute.py
@@ -1,28 +1,23 @@
 import os
-import sys
 
 with open('result1.txt', 'a') as file:
     file.write('start\n')
 file.close()
 with open('a.txt') as f:
     for line in f:
-        os.system('python3.8 ex.py http://'+line.strip('\n'))
+        os.system('proxychains python papercut.py http://'+line.strip('\n'))
         with open('result1.txt', 'a') as file:
             file.write(line.strip('\n')+'\n')
         file.close()
 with open('result1.txt', 'a') as file:
     file.write('fin\n')
 file.close()
-
-with open('result2.txt', 'a') as file:
-    file.write('start\n')
-file.close()
-with open('aa.txt') as f:
+with open('a1.txt') as f:
     for line in f:
-        os.system('python3.8 ex.py http://'+line.strip('\n'))
-        with open('result1.txt', 'a') as file:
+        os.system('proxychains python papercut.py http://'+line.strip('\n'))
+        with open('result2.txt', 'a') as file:
             file.write(line.strip('\n')+'\n')
         file.close()
-with open('result1.txt', 'a') as file:
+with open('result2.txt', 'a') as file:
     file.write('fin\n')
 file.close()
